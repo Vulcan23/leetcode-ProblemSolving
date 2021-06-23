e257e9b89cf18ef64fcd24d7e8e289945be8851d
yuanshi <1075552122@qq.com>
2021-06-23 17:29:01 +0800

    刷题记录

---
+++ 6. Z 字形变换.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} s
+ * @param {number} numRows
+ * @return {string}
+ */
+var convert = function (s, numRows) {
+    if (numRows === 1) {
+        return s;
+    }
+    let result = [];
+    for (let i = 0; i < numRows; i++) {
+        result.push([]);
+    }
+    let index = 0;
+    let direction = 1;
+    for (let i = 0; i < s.length; i++) {
+        result[index].push(s[i]);
+        if (index === 0) {
+            direction = 1;
+        } else if (index === numRows - 1) {
+            direction = -1;
+        }
+        index += direction;
+    }
+    return result.reduce((accumulator, currentValue) => accumulator + currentValue.join(""), "");
+};
+
+console.log(convert("PAYPALISHIRING", 3));
+console.log(convert("PAYPALISHIRING", 4));
+console.log(convert("A", 1));
