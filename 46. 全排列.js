da085d0782e9059951186cef5d9a5e3a7ee2e26a
yuanshi <1075552122@qq.com>
2021-07-08 20:25:34 +0800

    刷题记录

---
+++ 46. 全排列.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permute = function (nums) {
+    this.result = [];
+    dfs(nums, []);
+    return result;
+};
+
+function dfs(arrRest, arr) {
+    if (arrRest.length === 0) {
+        return this.result.push(arr.slice());
+    }
+    for (let i = 0; i < arrRest.length; i++) {
+        let arrClone = arrRest.slice();
+        arr.push(arrClone.splice(i, 1)[0]);
+        dfs(arrClone, arr);
+        arr.pop();
+    }
+}
+
+console.log(permute([1, 2, 3]));
+console.log(permute([0, 1]));
+console.log(permute([1]));
