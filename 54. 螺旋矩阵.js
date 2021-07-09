504b3c031744507614d51e3037df8033ad789719
yuanshi <1075552122@qq.com>
2021-07-09 23:01:42 +0800

    针对数组原地操作的思想，很少见，高效简洁

---
+++ 54. 螺旋矩阵.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number[][]} matrix
+ * @return {number[]}
+ */
+var spiralOrder = function (matrix) {
+    let arr = matrix.splice(0, 1)[0];
+    while (matrix.length) {
+        let temp = [];
+        for (let i = 0; i < matrix[0].length; i++) {
+            temp.push([]);
+        }
+        for (let i = matrix[0].length - 1; i >= 0; i--) {
+            for (let j = 0; j < matrix.length; j++) {
+                temp[matrix[0].length - 1 - i].push(matrix[j][i]);
+            }
+        }
+        matrix = temp;
+        arr.push(...matrix.splice(0, 1)[0]);
+    }
+    return arr;
+};
+
+let matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+];
+console.log(spiralOrder(matrix));
+
+matrix = [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12],
+];
+console.log(spiralOrder(matrix));
