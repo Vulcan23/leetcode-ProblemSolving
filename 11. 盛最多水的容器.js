731d747c26083206bc535e051052b087fcf72ccd
yuanshi <1075552122@qq.com>
2021-07-09 09:50:57 +0800

    刷题记录

--- 11. 盛最多水的容器.js
+++ 11. 盛最多水的容器.js
@@ -6,15 +6,15 @@ var maxArea = function (height) {
     let max = 0,
         left = 0,
         right = height.length - 1;
-    while (left < right) {
+    do {
         let sum;
         if (height[left] < height[right]) {
             sum = height[left] * (right - left++);
         } else {
             sum = height[right] * (right-- - left);
         }
-        sum > max && (max = sum);
-    }
+        max = Math.max(sum, max);
+    } while (left < right);
     return max;
 };
 
--- 48. 旋转图像.js
+++ 48. 旋转图像.js
@@ -3,7 +3,7 @@
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
 var rotate = function (matrix) {
-
+    
 };
 
 console.log(rotate([[1, 2, 3], [4, 5, 6], [7, 8, 9]]));
