16ec5892391ca1e6e517637d2270d0b5184314da
yuanshi <1075552122@qq.com>
2021-07-11 15:42:30 +0800

    刷题记录

--- 1514. 概率最大的路径.js
+++ 1514. 概率最大的路径.js
@@ -16,22 +16,21 @@ var maxProbability = function (n, edges, succProb, start, end) {
         let [i, j] = value;
         graph[i][j] = graph[j][i] = succProb[index];
     });
-    let stack = [[start, 1]];
-    let prob = [];
+    let stack = [[start, 1]],
+        item,
+        prob = [];
     prob[start] = 1;
-    while (stack.length) {
-        let [node, _prob] = stack.pop();
-        if (_prob < prob[node]) {
-            continue;
-        }
-        let newAdd = false;
-        for (let i in graph[node]) {
-            if (_prob * graph[node][i] > (prob[i] || 0)) {
-                stack.push([i, prob[i] = _prob * graph[node][i]]);
-                newAdd = true;
+    while (item = stack.pop()) {
+        if (item[1] >= prob[item[0]]) {
+            let newAdd = false;
+            for (let i in graph[item[0]]) {
+                if (item[1] * graph[item[0]][i] > (prob[i] || 0)) {
+                    stack.push([i, prob[i] = item[1] * graph[item[0]][i]]);
+                    newAdd = true;
+                }
             }
+            newAdd && stack.sort((a, b) => a[1] - b[1]);
         }
-        newAdd && stack.sort((a, b) => a[1] - b[1]);
     }
     return prob[end] || 0;
 };
--- 56. 合并区间.js
+++ 56. 合并区间.js
@@ -3,7 +3,19 @@
  * @return {number[][]}
  */
 var merge = function (intervals) {
-    
+    intervals.sort((a, b) => a[0] - b[0]);
+    let arr = [],
+        prev = intervals[0];
+    for (let i = 1; i < intervals.length; i++) {
+        if (intervals[i][0] <= prev[1]) {
+            prev[1] = Math.max(prev[1], intervals[i][1]);
+        } else {
+            arr.push(prev);
+            prev = intervals[i];
+        }
+    }
+    arr.push(prev);
+    return arr;
 };
 
 console.log(merge([[1, 3], [2, 6], [8, 10], [15, 18]]));
