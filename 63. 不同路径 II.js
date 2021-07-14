92b89eef10318bd9ba3efb441c9ac2ffb3ac720a
yuanshi <1075552122@qq.com>
2021-07-14 10:52:56 +0800

    刷题记录

--- 63. 不同路径 II.js
+++ 63. 不同路径 II.js
@@ -3,7 +3,27 @@
  * @return {number}
  */
 var uniquePathsWithObstacles = function (obstacleGrid) {
-    
+    const rowLength = obstacleGrid[0].length,
+        columnLength = obstacleGrid.length,
+        graph = Array(columnLength).fill().map(() => Array(rowLength).fill(0));
+    for (let i = 0; i < rowLength; i++) {
+        if (obstacleGrid[0][i]) {
+            break;
+        }
+        graph[0][i] = 1;
+    }
+    for (let i = 0; i < columnLength; i++) {
+        if (obstacleGrid[i][0]) {
+            break;
+        }
+        graph[i][0] = 1;
+    }
+    for (let i = 1; i < columnLength; i++) {
+        for (let j = 1; j < rowLength; j++) {
+            !obstacleGrid[i][j] && (graph[i][j] = graph[i - 1][j] + graph[i][j - 1]);
+        }
+    }
+    return graph[columnLength - 1][rowLength - 1];
 };
 
 let obstacleGrid = [
