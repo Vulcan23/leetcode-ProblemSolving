a88f23267d4a5bfc383c60857441a0f0677d4b4d
yuanshi <1075552122@qq.com>
2021-07-17 01:22:50 +0800

    刷题记录

---
+++ 77. 组合.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {number[][]}
+ */
+var combine = function (n, k) {
+    this.result = [],
+        this.k = k;
+    dfs([], n);
+    return result;
+};
+
+function dfs(arr, n) {
+    if (arr.length + n === this.k) {
+        this.result.push(arr.concat(Array(n).fill().map((v, i) => i + 1)));
+        return true;
+    }
+    if (arr.length === this.k) {
+        this.result.push(arr.slice());
+    } else {
+        for (let i = n; i > 0; i--) {
+            arr.push(i);
+            if (dfs(arr, i - 1)) {
+                arr.pop();
+                break;
+            }
+            arr.pop();
+        }
+    }
+}
+
+console.log(combine(6, 4));
