2c4940148bd5934899aa22de9ec15bf31a04a871
yuanshi <1075552122@qq.com>
2021-07-17 21:24:54 +0800

    刷题记录

--- 81. 搜索旋转排序数组 II.js
+++ 81. 搜索旋转排序数组 II.js
@@ -34,6 +34,3 @@ var search = function (nums, target) {
 
 console.log(search([2, 5, 6, 0, 0, 1, 2], 0));
 console.log(search([2, 5, 6, 0, 0, 1, 2], 3));
-
-console.log(search([1, 1, 1, 0, 1], 0));
-console.log(search([1, 0, 1, 1, 1], 0));
