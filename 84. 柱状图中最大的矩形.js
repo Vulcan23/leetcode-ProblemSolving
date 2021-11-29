115d2d3e5435ca4ac57a35ce93d175cf419d6479
yuanshi <1075552122@qq.com>
2021-11-29 16:26:10 +0800

    刷题记录

--- 84. 柱状图中最大的矩形.js
+++ 84. 柱状图中最大的矩形.js
@@ -3,8 +3,18 @@
  * @return {number}
  */
 var largestRectangleArea = function (heights) {
-
+    heights.unshift(0);
+    heights.push(0);
+    let stack = [0];
+    let res = 0;
+    for (let i = 1; i < heights.length; i++) {
+        while (heights[i] < heights[stack[stack.length - 1]]) {
+            res = Math.max(heights[stack.pop()] * (i - stack[stack.length - 1] - 1), res);
+        }
+        stack.push(i);
+    }
+    return res;
 };
 
-console.log([2, 1, 5, 6, 2, 3]);
-console.log([2, 4]);
+console.log(largestRectangleArea([2, 1, 5, 6, 2, 3]));
+console.log(largestRectangleArea([2, 4]));
